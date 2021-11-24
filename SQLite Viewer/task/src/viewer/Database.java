cd05829ffad135ea250abc0356a105691c30efcb
Pradeep Shanbhogue <warriopradeep@gmail.com>
2021-11-25 00:04:53 +0530

    Initial commit

---
+++ SQLite Viewer/task/src/viewer/Database.java
@@ -0,0 +1,73 @@
+package viewer;
+
+import org.sqlite.SQLiteDataSource;
+
+import javax.swing.table.TableModel;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database implements AutoCloseable {
+
+    public static final String ALL_ROWS_QUERY = "SELECT * FROM %s;";
+
+    private Connection connection;
+
+    public Database(String fileName) throws SQLException {
+        connect(fileName);
+    }
+
+    private void connect(String fileName) throws SQLException {
+        final String url = "jdbc:sqlite:%s";
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(String.format(url, fileName));
+        connection = dataSource.getConnection();
+    }
+
+    public List<String> getTableNames() throws SQLException {
+        final String query = "SELECT name FROM sqlite_master " +
+                "WHERE type ='table' AND name NOT LIKE 'sqlite_%'";
+        List<String> list = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                list.add(name);
+            }
+            return list;
+        }
+    }
+
+    public TableModel getTableModel(String query) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            ResultSetMetaData metaData = resultSet.getMetaData();
+
+            int columnCount = metaData.getColumnCount();
+            String[] columns = new String[columnCount];
+            for (int i = 0; i < metaData.getColumnCount(); i++) {
+                columns[i] = metaData.getColumnName(i + 1);
+            }
+
+            Map<Integer, Object[]> data = new HashMap<>();
+            int i = 0;
+            while (resultSet.next()) {
+                Object[] row = new Object[columnCount];
+                for (int j = 0; j < columnCount; j++) {
+                    row[j] = resultSet.getObject(j + 1);
+                }
+                data.put(i++, row);
+            }
+            return new DataTableModel(columns, data);
+        }
+
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.connection.close();
+    }
+
+}
---
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -0,0 +1,154 @@
+package viewer;
+
+import viewer.workers.DataLoader;
+import viewer.workers.TableNamesLoader;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SQLiteViewer extends JFrame {
+
+    public SQLiteViewer() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setResizable(true);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+        setTitle("SQLite Viewer");
+        initComponents();
+        setVisible(true);
+    }
+
+    private void initComponents() {
+        Font jetbrainsMonoFont = new Font("Jetbrains Mono", Font.BOLD, 14);
+
+        Insets topLeft8Padding = new Insets(8, 8, 0, 0);
+        Insets top8Padding = new Insets(8, 0, 0, 0);
+        Insets left8Padding = new Insets(0, 8, 0, 0);
+
+        JPanel topFormPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        topFormPanel.setBorder(new EmptyBorder(8, 8, 8, 8));
+
+        JLabel fileNameLabel = new JLabel("Filename");
+        fileNameLabel.setFont(jetbrainsMonoFont);
+        constraints.weightx = 0.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        topFormPanel.add(fileNameLabel, constraints);
+
+        JTextField fileNameTextField = new JTextField();
+        fileNameLabel.setLabelFor(fileNameTextField);
+        fileNameTextField.setName("FileNameTextField");
+        fileNameTextField.setFont(jetbrainsMonoFont.deriveFont(Font.PLAIN));
+        fileNameTextField.setColumns(20);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.insets = left8Padding;
+        constraints.weightx = 1.0;
+        constraints.gridx = 1;
+        topFormPanel.add(fileNameTextField, constraints);
+
+        JButton openFileButton = new JButton("Open");
+        openFileButton.setName("OpenFileButton");
+        openFileButton.setFont(jetbrainsMonoFont);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.0;
+        constraints.gridx = 2;
+        topFormPanel.add(openFileButton, constraints);
+
+        JLabel tableLabel = new JLabel("Table");
+        tableLabel.setFont(jetbrainsMonoFont);
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.insets = top8Padding;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        topFormPanel.add(tableLabel, constraints);
+
+        JComboBox<String> tableSelect = new JComboBox<>();
+        tableLabel.setLabelFor(tableSelect);
+        tableSelect.setName("TablesComboBox");
+        tableSelect.setFont(jetbrainsMonoFont);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.insets = topLeft8Padding;
+        constraints.weightx = 1.0;
+        constraints.gridx = 1;
+        topFormPanel.add(tableSelect, constraints);
+
+        JLabel queryLabel = new JLabel("Query");
+        queryLabel.setFont(jetbrainsMonoFont);
+        constraints.fill = GridBagConstraints.NONE;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = top8Padding;
+        constraints.weightx = 0.0;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        topFormPanel.add(queryLabel, constraints);
+
+        JTextArea queryTextArea = new JTextArea();
+        queryLabel.setLabelFor(queryTextArea);
+        queryTextArea.setName("QueryTextArea");
+        queryTextArea.setRows(5);
+        queryTextArea.setFont(jetbrainsMonoFont.deriveFont(Font.PLAIN));
+        queryTextArea.setWrapStyleWord(true);
+        queryTextArea.setEnabled(false);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.insets = topLeft8Padding;
+        constraints.weightx = 1.0;
+        constraints.gridx = 1;
+        JScrollPane queryTextScroll = new JScrollPane(queryTextArea);
+        topFormPanel.add(queryTextScroll, constraints);
+
+        JButton executeQueryButton = new JButton("Execute");
+        executeQueryButton.setName("ExecuteQueryButton");
+        executeQueryButton.setFont(jetbrainsMonoFont);
+        executeQueryButton.setEnabled(false);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = topLeft8Padding;
+        constraints.weightx = 0.0;
+        constraints.gridx = 2;
+        topFormPanel.add(executeQueryButton, constraints);
+
+        add(topFormPanel, BorderLayout.PAGE_START);
+
+        JTable dataTable = new JTable();
+        dataTable.setName("Table");
+        dataTable.setFont(jetbrainsMonoFont.deriveFont(Font.PLAIN));
+        dataTable.setFillsViewportHeight(true);
+        JScrollPane tableScrollPane = new JScrollPane(dataTable);
+        tableScrollPane.setBorder(new EmptyBorder(8, 8, 8, 8));
+        add(tableScrollPane, BorderLayout.CENTER);
+
+
+        openFileButton.addActionListener(event -> {
+            String fileName = fileNameTextField.getText();
+            if (Files.exists(Paths.get(fileName))) {
+                new TableNamesLoader(fileName, tableSelect).execute();
+                queryTextArea.setEnabled(true);
+                executeQueryButton.setEnabled(true);
+                queryTextArea.setText(String.format(Database.ALL_ROWS_QUERY, tableSelect.getSelectedItem()));
+            } else {
+                tableSelect.removeAllItems();
+                queryTextArea.setText(null);
+                queryTextArea.setEnabled(false);
+                executeQueryButton.setEnabled(false);
+                dataTable.setModel(new DefaultTableModel());
+                JOptionPane.showMessageDialog(new Frame(), "File doesn't exist!", "File Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        tableSelect.addItemListener(event -> queryTextArea.setText(
+                String.format(Database.ALL_ROWS_QUERY, event.getItem().toString())));
+
+        executeQueryButton.addActionListener(event -> new DataLoader(
+                fileNameTextField.getText(),
+                queryTextArea.getText(),
+                dataTable).execute());
+    }
+}
